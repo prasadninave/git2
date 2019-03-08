0cf7fba0f00b82764e00d41d9c9939bfd6ef5616
Prasad Ninave <prasadninave11@gmail.com>
2019-03-08 13:17:40 +0530

    First commit

---
+++ ExcelperationDemo3/src/com/model/Exceldataconfig.java
@@ -0,0 +1,26 @@
+package com.model;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+
+
+public class Exceldataconfig {
+	
+	
+	public void Exceldata(String file) throws FileNotFoundException
+	{
+		XSSFWorkbook wb;
+		XSSFSheet sheet;
+		FileInputStream fis= new FileInputStream(file);
+		
+		
+		
+	
+	}
+		
+
+}
